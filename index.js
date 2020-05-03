3b921f00e45b56d564509225815cf7344d39751b
Joshua Benjamin <jbenj@sas.upenn.edu>
2020-05-03 19:36:32 -0400

    created classes for manager, engineer, intern; push info to new team

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const inquirer = require('inquirer');
 const fs = require("fs");
 const htmlRender = require("./lib/htmlRenderer")
+const Manager = require("./lib/Manager")
+const Engineer = require("./lib/Engineer")
+const Intern = require("./lib/Intern")
 // const generateMarkdown = require("generateMarkdown")
 const team = []
 
@@ -29,13 +32,13 @@ const managerQs = [
     },
     {
         type: "input",
-        name: "managerOffice",
-        message: "What is the manager's office number?"
+        name: "managerEmail",
+        message: "What is the manager's email address?"
     },
     {
         type: "input",
-        name: "managerEmail",
-        message: "What is the manager's email address?"
+        name: "managerOffice",
+        message: "What is the manager's office number?"
     }
 ];
 
@@ -53,13 +56,13 @@ const engineerQs = [
     },
     {
         type: "input",
-        name: "engineerGHUN",
-        message: "What is the engineer's github.com User Name?"
+        name: "engineerEmail",
+        message: "What is the engineer's email address?"
     },
     {
         type: "input",
-        name: "engineerEmail",
-        message: "What is the engineer's email address?"
+        name: "engineerGHUN",
+        message: "What is the engineer's github.com User Name?"
     }
 ];
 
@@ -77,13 +80,13 @@ const internQs = [
     },
     {
         type: "input",
-        name: "internSchool",
-        message: "Where does the intern study?"
+        name: "internEmail",
+        message: "What is the intern's email address?"
     },
     {
         type: "input",
-        name: "internEmail",
-        message: "What is the intern's email address?"
+        name: "internSchool",
+        message: "Where does the intern study?"
     }
 ];
 
@@ -96,6 +99,7 @@ const internQs = [
 //         choices: ["Manager", "Engineer", "Intern", "No one else for now"]
 //     },
 // ];
+//============================
 
 //======not necessary for now
 // function writeToFile(fileName, data) {
@@ -128,40 +132,54 @@ const askIQuestions = async (internQs) => {
     // console.log(answers);
     return answers;
 };
-const askFQuestions = async (finalQs) => {
-    const answers = await inquirer.prompt(finalQs).catch(err => err)
-    // console.log(answers);
-    return answers;
-};
+// const askFQuestions = async (finalQs) => {
+//     const answers = await inquirer.prompt(finalQs).catch(err => err)
+//     // console.log(answers);
+//     return answers;
+// };
+//===========================
 
 //=========CALL QUESTIONS
 const init = async () => {
     const userAnswers = await askInitQuestions(initialQs);
 
-    const {addTeamMember} = userAnswers
-    if(addTeamMember === 'Manager') {
+    const { addTeamMember } = userAnswers
+    if (addTeamMember === 'Manager') {
         const userAnswers = await askMQuestions(managerQs);
-    
-
+        console.log(userAnswers)
+        const newMan = new Manager(userAnswers.managerName, userAnswers.managerID, userAnswers.managerEmail, userAnswers.managerOffice);
+        console.log(newMan);
+        team.push(newMan);
+        console.log(team);
         //create new M class
 
         //once created push to an array 
 
-    }else if(addTeamMember === 'Engineer'){
+    } else if (addTeamMember === 'Engineer') {
         const userAnswers = await askEQuestions(engineerQs);
+        console.log(userAnswers)
+        const newEng = new Engineer (userAnswers.engineerName, userAnswers.engineerID, userAnswers.engineerEmail, userAnswers.engineerGHUN);
+        console.log(newEng);
+        team.push(newEng);
+        console.log(team);
 
-    }else if (addTeamMember === "Intern"){
+    } else if (addTeamMember === "Intern") {
         const userAnswers = await askIQuestions(internQs);
+        console.log(userAnswers)
+        const newInt = new Intern (userAnswers.internName, userAnswers.internID, userAnswers.internEmail, userAnswers.internSchool);
+        console.log(newInt);
+        team.push(newInt);
+        console.log(team);
 
-    }else{
+    } else {
         const html = htmlRender(team)
 
     }
-    }
+}
 
 
-    //    console.log(userAnswers);
-    // writeToFile("exampleREADME.md", userAnswers)
+//    console.log(userAnswers);
+// writeToFile("exampleREADME.md", userAnswers)
 
 //========ANSWERS TO HTML FILES
 
@@ -171,4 +189,4 @@ const init = async () => {
 //     writeToFile("exampleREADME.md", userAnswers)
 //          }
 
- init();
+init();
